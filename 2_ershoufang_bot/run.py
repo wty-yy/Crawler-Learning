27c8b9b5aaf4cf0036bc46849b8b02f6b17f5ca5
wty <993660140@qq.com>
2022-08-02 13:07:03 +0800

    update ershoufang_bot

--- 2_ershoufang_bot/run.py
+++ 2_ershoufang_bot/run.py
@@ -5,7 +5,7 @@ from mybot.bot import Bot
 if __name__ == '__main__':
     st = int(input('起始页：'))
     en = int(input('终止页：'))
-    with Bot(driver_path=r'D:\SeleniumDriver\chromedriver.exe',
+    with Bot(driver_path=r'C:\SeleniumDriver\chromedriver.exe',
              pg=(st, en), teardown=False) as bot:
         for i in range(st, en+1):
             bot.load_page(i)
